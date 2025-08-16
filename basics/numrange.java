5ae83b4f5866886313c3aa152b2eadfd4c972c43
Prithavi01 <prithaviap@gmail.com>
2025-08-16 22:58:47 +0530

    Updated

--- basics/numrange.java
+++ basics/numrange.java
@@ -5,8 +5,9 @@ class numrange{
         int a=s.nextInt();
         int b=s.nextInt();
         int c=s.nextInt();
-        if(a>=b && a>=c)
-           int greater=a;
-        elseif
+        if ((a > b && a < c) || (a > c && a < b))
+            System.out.println(a + " lies between " + b + " and " + c);
+        else 
+            System.out.println(a + " does not lie between " + b + " and " + c);
     }
 }
